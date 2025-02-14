c5fe1a694f5f527f86ae1d1a86ab0449c09486d6
CarlosMatam <carlosmatamorosluna25@gmail.com>
2025-02-14 13:50:51 -0600

    Primer commit

---
+++ src/app/components/FloatingChatButton.tsx
@@ -0,0 +1,8 @@
+const FloatingChatButton = () => {
+    return (
+      <div className="fixed bottom-4 right-4 bg-blue-500 text-white p-4 rounded-full shadow-lg cursor-pointer">
+        💬
+      </div>
+    );
+  };
+  
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,13 @@
+export function Navbar() {
+    return (
+      <nav className="bg-blue-900 text-white py-4">
+        <div className="container mx-auto flex justify-end space-x-6 text-sm">
+          <a href="#" className="hover:underline">INICIO</a>
+          <a href="#" className="hover:underline">CURSOS</a>
+          <a href="#" className="hover:underline">INSTRUCTORES</a>
+          <a href="#" className="hover:underline">TESTIMONIOS</a>
+          <a href="#" className="hover:underline">CONTACTO</a>
+        </div>
+      </nav>
+    );
+  }
